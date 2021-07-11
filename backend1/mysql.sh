c95fc455633f11cfa8bfcbcdb147dff9c0f1006c
JaeHongDev <println@kakao.com>
2021-07-11 14:48:17 +0000

    20210711

--- backend1/mysql.sh
+++ backend1/mysql.sh
@@ -6,7 +6,8 @@
 command sudo apt update
 command sudo apt-get install mysql-server
 command sudo service mysql start
+command sudo mysql -u root mysql -e "create database TEST"
+command sudo mysql -u root mysql -e "create database DEV"
 command sudo mysql -u root mysql -e "alter user 'root'@'localhost' identified with mysql_native_password by 'root'";
-command sudo mysql -u root -p root mysql -e "create database test"
 
   
