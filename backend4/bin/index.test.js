e35c784866c93653f87443f49bb1eb772cbe971c
JaeHongDev <println@kakao.com>
2021-08-09 13:30:12 +0000

    다대다 테스트 과정에서 문제 발생

---
+++ backend4/bin/index.test.js
@@ -0,0 +1,10 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+var index_1 = __importDefault(require("./index"));
+test("adds 1 + 2 to equal 3", function () {
+    expect(index_1.default(1, 2)).toBe(3);
+});
+//# sourceMappingURL=index.test.js.map
--- backend4/src/index.ts
+++ backend4/src/index.ts
@@ -1,4 +1,73 @@
-function sum(a: number, b: number) {
-  return a + b;
+import {
+  createConnection,
+  Entity,
+  ManyToOne,
+  OneToMany,
+  PrimaryGeneratedColumn,
+} from "typeorm";
+
+// user Entity
+@Entity()
+class User {
+  @PrimaryGeneratedColumn()
+  id!: number;
+
+  @OneToMany(() => ReadMyBook, (table) => table.users)
+  readMyBooks!: ReadMyBook[];
 }
-export default sum;
+
+// community entity
+@Entity()
+class Community {
+  @PrimaryGeneratedColumn()
+  id!: number;
+}
+
+//book entity
+@Entity()
+class Book {
+  @PrimaryGeneratedColumn()
+  id!: number;
+
+  @OneToMany(() => ReadMyBook, (table) => table.)
+  readMyBooks!: ReadMyBook[];
+}
+
+// ReadMyBook entity
+@Entity()
+class ReadMyBook {
+  @PrimaryGeneratedColumn()
+  id!: number;
+
+  @ManyToOne(() => User, (user) => user.readMyBooks)
+  users!: User[];
+
+  @ManyToOne(() => Book, (book) => book.readMyBooks)
+  books!: Book[]
+}
+
+// user_commnuty entity
+class UserCommunity {
+
+  @PrimaryGeneratedColumn()
+  id!: number;
+
+  
+}
+
+
+async function main() {
+  const connection = await createConnection({
+    type: "mysql",
+    host: "localhost",
+    port: 3306,
+    username: "root",
+    password: "root",
+    database: "TEST",
+  });
+
+  const data = await connection.query("show databases");
+  console.log(data);
+}
+
+main();
